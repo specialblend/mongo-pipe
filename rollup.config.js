eb473e37e12c17efb6087fadfd707a07f2dd475e
specialblend <cj@specialblend.net>
2019-06-15 15:30:04 -0500

    setup exports for util functions

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,30 @@ import packageJSON from './package.json';
 export default [
     {
         input: 'src/main.js',
-        output: {
-            name: 'index',
-            file: packageJSON.browser,
-            format: 'umd',
-        },
-    },
-    {
-        input: 'src/main.js',
         output: [
             {
+                name: 'main',
                 file: packageJSON.main,
                 format: 'cjs',
             },
             {
+                name: 'main',
                 file: packageJSON.module,
+                format: 'es',
+            },
+        ],
+    },
+    {
+        input: 'src/util/index.js',
+        output: [
+            {
+                name: 'util',
+                file: 'dist/util.cjs.js',
+                format: 'cjs',
+            },
+            {
+                name: 'util',
+                file: 'dist/util.esm.js',
                 format: 'es',
             },
         ],
