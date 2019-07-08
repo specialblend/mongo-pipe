a6600f671f632cf3ccd7407b9c18a51e38a1cebf
specialblend <cj@specialblend.net>
2019-07-07 20:12:28 -0500

    housekeeping

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+/* eslint-disable padded-blocks */
+
 import {
     compose,
     converge,
@@ -21,7 +23,7 @@ import {
 import { pipeSpecs } from './common';
 
 const isArray = Array.isArray;
-const isOk = pathSatisfies(Boolean, ['result', 'ok']);
+const opIsOk = pathSatisfies(Boolean, ['result', 'ok']);
 const flattenChild = when(is(Object), unless(isArray, flatten));
 const flattenChildren = map(flattenChild);
 const getOps = prop('ops');
@@ -33,27 +35,49 @@ const toArray = Array.from;
 
 export const withSetProps = compose(withSet, flattenChildren, withoutId);
 
+/**
+ * Call primary async handler and return
+ * if first result satisfies predicate, otherwise
+ * call secondary handler
+ * @type {Function}
+ */
 const eitherAsync = curry(
-    (primary, secondary) =>
+    (predicate, primary, secondary) =>
         async(...payload) => {
             const response = await primary(...payload);
-            if (isOk(response)) {
+            if (predicate(response)) {
                 return response;
             }
             return secondary(...payload);
         }
 );
 
+/**
+ * Add helper methods to collection object
+ * @param {Object} collection Mongo collection object
+ * @returns {object} collection with helper methods
+ */
 const withHelperMethods = collection => {
+
+    /**
+     * Extract native methods
+     */
     const { find, findOne, updateOne, insertOne, remove } = collection;
 
+    /**
+     * Local helpers
+     */
+    const updatePropsWithId = converge(updateOne, [withId, withSetProps]);
+
+    /**
+     * Exported helper methods
+     */
     const all = pipe(find, then(toArray));
     const createOne = pipe(insertOne, then(getFirstOp));
     const findOneById = useWith(findOne, [withId]);
     const removeOneById = pipe(withId, remove);
-    const updatePropsWithId = converge(updateOne, [withId, withSetProps]);
     const updateOneById = pipe(updatePropsWithId, then(getFirstOp));
-    const upsertOneById = pipe(eitherAsync(updatePropsWithId, insertOne), then(getFirstOp));
+    const upsertOneById = pipe(eitherAsync(opIsOk, updatePropsWithId, insertOne), then(getFirstOp));
 
     const pipeTransformers = unapply(pipeSpecs(collection));
 
