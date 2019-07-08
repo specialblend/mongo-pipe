bca9033d6f01042a241e15d87b87e8b595b9a531
specialblend <cj@specialblend.net>
2019-07-07 20:14:25 -0500

    housekeeping

--- src/mongo.js
+++ src/mongo.js
@@ -12,10 +12,21 @@ import { nativeSpecMethods } from './manifest';
  */
 const explicateNativeMethods = converge(map, [bindTo, pick(nativeSpecMethods)]);
 
+/**
+ * Setup collection
+ * @type {Function}
+ */
 const setupCollection = pipe(explicateNativeMethods, withHelperMethods);
 
+/**
+ * Memoized connector
+ */
 const connect = memoizeAll(MongoClient.connect);
 
+/**
+ * Handle Mongo connection
+ * @type {Function}
+ */
 const withConnection = curry(
     (connection, dbName, collectionName) =>
         setupCollection(
@@ -25,5 +36,10 @@ const withConnection = curry(
         ),
 );
 
+/**
+ * Connect to Mongo and return a curried connection handler
+ * @type {Function}
+ */
 const mongo = pipe(connect, then(withConnection));
+
 export default mongo;
