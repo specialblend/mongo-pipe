e4ce26ec324db0d65bc94e55765724f0ca169eb8
specialblend <cj@specialblend.net>
2019-07-07 20:54:11 -0500

    export mocks

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,27 @@
 import packageJSON from './package.json';
 
-export default
-{
-    input: 'src/mongo.js',
-    output: [
-        {
-            name: 'mongo',
-            file: packageJSON.main,
+export default [
+    {
+        input: 'src/mongo.js',
+        output: [
+            {
+                name: 'mongo',
+                file: packageJSON.main,
+                format: 'cjs',
+            },
+            {
+                name: 'mongo',
+                file: packageJSON.module,
+                format: 'esm',
+            },
+        ],
+    },
+    {
+        input: '__mocks__/mongodb.js',
+        output: {
+            name: 'mongodb',
+            file: 'mock/mongodb.js',
             format: 'cjs',
         },
-        {
-            name: 'mongo',
-            file: packageJSON.module,
-            format: 'esm',
-        },
-    ],
-};
+    },
+];
